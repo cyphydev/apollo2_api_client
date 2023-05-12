8a62df173ecb229398434a0752d8e996342c3242
hrukalive <bowen.parallife@gmail.com>
2023-05-11 22:51:43 -0500

    Git setup

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from setuptools import setup, find_packages  # noqa: H301
 # http://pypi.python.org/pypi/setuptools
 NAME = "apollo2-api-client"
 VERSION = "1.0.0"
-PYTHON_REQUIRES = ">=3.7"
+PYTHON_REQUIRES = ">=3.8"
 REQUIRES = [
     "urllib3 >= 1.25.3",
     "python-dateutil",
